4678d05d8d5883ae37fd1c603e5b7973ae294f0d
CfopGod <112650836+CfopGod@users.noreply.github.com>
2022-10-03 13:59:36 -0400

    need to add blink function and border button functionality

---
+++ scripts.js
@@ -0,0 +1,53 @@
+// function look() {
+//     console.log("test")
+
+//     if (document.getElementById("topLeftDiv") == onmouseover) {
+//         console.log("test2")
+//     }
+// }
+
+
+function topLeftLook()  {
+    document.getElementById("eye1").src="img/eye_ul.gif"
+    document.getElementById("eye2").src="img/eye_ul.gif"
+}
+
+function topCenterLook() {
+    document.getElementById("eye1").src="img/eye_uc.gif"
+    document.getElementById("eye2").src="img/eye_uc.gif"
+}
+
+function topRightLook() {
+    document.getElementById("eye1").src="img/eye_ur.gif"
+    document.getElementById("eye2").src="img/eye_ur.gif"
+}
+
+function middleLeftLook() {
+    document.getElementById("eye1").src="img/eye_cl.gif"
+    document.getElementById("eye2").src="img/eye_cl.gif"
+}
+
+function CenterLook() {
+    document.getElementById("eye1").src="img/eye_c.gif"
+    document.getElementById("eye2").src="img/eye_c.gif"
+}
+
+function middleRightLook() {
+    document.getElementById("eye1").src="img/eye_cr.gif"
+    document.getElementById("eye2").src="img/eye_cr.gif"
+}
+
+function bottomLeftLook() {
+    document.getElementById("eye1").src="img/eye_ll.gif"
+    document.getElementById("eye2").src="img/eye_ll.gif"
+}
+
+function bottomCenterLook() {
+    document.getElementById("eye1").src="img/eye_lc.gif"
+    document.getElementById("eye2").src="img/eye_lc.gif"
+}
+
+function bottomRightLook() {
+    document.getElementById("eye1").src="img/eye_lr.gif"
+    document.getElementById("eye2").src="img/eye_lr.gif"
+}
